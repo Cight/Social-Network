5f97655ea5d9f549b0fece66dcf1cb337ca89f7a
cight <necronezum@gmail.com>
2017-04-12 00:52:54 -0400

    Added Image to Header next to Profle Dropdown Menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,9 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
+                               aria-expanded="false" style="position: relative; padding-left: 50px;">
+                                <img src="/uploads/avatars/{{ Auth::user()->avatar }}"
+                                     style="width: 32px; height: 32px; float: left; position: absolute; top: 10px; left: 10px; border-radius: 50%; "/>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
